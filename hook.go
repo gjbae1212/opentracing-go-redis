c0a45f5afadacf7ea3454b4ffa2c48e0d76e34d3
gjbae1212 <gjbae1212@gmail.com>
2019-12-07 00:13:49 +0900

    Add a tag of database to the hook.

--- hook.go
+++ hook.go
@@ -3,21 +3,28 @@ package otredis
 import (
 	"context"
 	"github.com/opentracing/opentracing-go/ext"
+	"strconv"
 	"strings"
 
 	"github.com/go-redis/redis/v7"
 	"github.com/opentracing/opentracing-go"
 )
 
+// reference: https://github.com/opentracing/specification/blob/master/semantic_conventions.md
+
 type hook struct {
-	addrs []string
+	addrs    []string
+	database int
 }
 
 // BeforeProcess is a hook before process.
 func (h hook) BeforeProcess(ctx context.Context, cmd redis.Cmder) (context.Context, error) {
 	span, newCtx := opentracing.StartSpanFromContext(ctx, "redis:cmd")
 	ext.DBType.Set(span, "redis")
+	ext.DBInstance.Set(span, strconv.Itoa(h.database))
 	ext.PeerAddress.Set(span, strings.Join(h.addrs, ", "))
+	ext.PeerService.Set(span, "redis")
+	ext.SpanKind.Set(span, ext.SpanKindEnum("client"))
 	ext.DBStatement.Set(span, strings.ToUpper(cmd.Name()))
 	return newCtx, nil
 }
@@ -35,7 +42,10 @@ func (h hook) AfterProcess(ctx context.Context, cmd redis.Cmder) error {
 func (h hook) BeforeProcessPipeline(ctx context.Context, cmds []redis.Cmder) (context.Context, error) {
 	span, newCtx := opentracing.StartSpanFromContext(ctx, "redis:pipeline:cmd")
 	ext.DBType.Set(span, "redis")
+	ext.DBInstance.Set(span, strconv.Itoa(h.database))
 	ext.PeerAddress.Set(span, strings.Join(h.addrs, ", "))
+	ext.PeerService.Set(span, "redis")
+	ext.SpanKind.Set(span, ext.SpanKindEnum("client"))
 	merge := make([]string, len(cmds))
 	for i, cmd := range cmds {
 		merge[i] = strings.ToUpper(cmd.Name())
--- hook_test.go
+++ hook_test.go
@@ -1,3 +1,170 @@
 package otredis
 
-// TODO: HOOK, mock redis, call test
+import (
+	"context"
+	"github.com/go-redis/redis/v7"
+	"github.com/opentracing/opentracing-go"
+	"testing"
+	"time"
+
+	"github.com/opentracing/opentracing-go/ext"
+	"github.com/opentracing/opentracing-go/mocktracer"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHook_BeforeProcess(t *testing.T) {
+	assert := assert.New(t)
+	opentracing.SetGlobalTracer(mocktracer.New())
+
+	tests := map[string]struct {
+		ctx context.Context
+		hk  hook
+		cmd redis.Cmder
+	}{
+		"success": {
+			ctx: context.TODO(),
+			hk:  hook{addrs: []string{"127.0.0.1:6379", "127.0.0.1:6378"}, database: 10},
+			cmd: redis.NewStringCmd("GET", "ALLAN"),
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			newCtx, err := tc.hk.BeforeProcess(tc.ctx, tc.cmd)
+			assert.NoError(err)
+			span := opentracing.SpanFromContext(newCtx)
+			tags := span.(*mocktracer.MockSpan).Tags()
+			for k, v := range tags {
+				switch k {
+				case string(ext.DBType):
+					assert.Equal("redis", v.(string))
+				case string(ext.DBInstance):
+					assert.Equal("10", v.(string))
+				case string(ext.PeerAddress):
+					assert.Equal("127.0.0.1:6379, 127.0.0.1:6378", v)
+				case string(ext.PeerService):
+					assert.Equal("redis", v.(string))
+				case string(ext.SpanKind):
+					assert.Equal("client", string(v.(ext.SpanKindEnum)))
+				case string(ext.DBStatement):
+					assert.Equal("GET", v)
+				default:
+					panic("unknown tag")
+				}
+			}
+		})
+	}
+}
+
+func TestHook_AfterProcess(t *testing.T) {
+	assert := assert.New(t)
+	opentracing.SetGlobalTracer(mocktracer.New())
+
+	tests := map[string]struct {
+		ctx context.Context
+		hk  hook
+		cmd redis.Cmder
+	}{
+		"success": {
+			ctx: context.TODO(),
+			hk:  hook{addrs: []string{"127.0.0.1:6379", "127.0.0.1:6378"}, database: 10},
+			cmd: redis.NewStringCmd("GET", "ALLAN"),
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			// before
+			newCtx, err := tc.hk.BeforeProcess(tc.ctx, tc.cmd)
+			assert.NoError(err)
+			now := time.Now()
+			time.Sleep(1 * time.Millisecond)
+
+			// after
+			err = tc.hk.AfterProcess(newCtx, tc.cmd)
+			assert.NoError(err)
+			span := opentracing.SpanFromContext(newCtx).(*mocktracer.MockSpan)
+			assert.True(span.FinishTime.UnixNano() > now.UnixNano())
+		})
+	}
+}
+
+func TestHook_BeforeProcessPipeline(t *testing.T) {
+	assert := assert.New(t)
+	opentracing.SetGlobalTracer(mocktracer.New())
+
+	tests := map[string]struct {
+		ctx  context.Context
+		hk   hook
+		cmds []redis.Cmder
+	}{
+		"success": {
+			ctx:  context.TODO(),
+			hk:   hook{addrs: []string{"127.0.0.1:6379", "127.0.0.1:6378"}, database: 10},
+			cmds: []redis.Cmder{redis.NewStringCmd("GET", "ALLAN"), redis.NewStringCmd("SET", "ALLAN")},
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			newCtx, err := tc.hk.BeforeProcessPipeline(tc.ctx, tc.cmds)
+			assert.NoError(err)
+			span := opentracing.SpanFromContext(newCtx)
+			tags := span.(*mocktracer.MockSpan).Tags()
+			for k, v := range tags {
+				switch k {
+				case string(ext.DBType):
+					assert.Equal("redis", v.(string))
+				case string(ext.DBInstance):
+					assert.Equal("10", v.(string))
+				case string(ext.PeerAddress):
+					assert.Equal("127.0.0.1:6379, 127.0.0.1:6378", v)
+				case string(ext.PeerService):
+					assert.Equal("redis", v.(string))
+				case string(ext.SpanKind):
+					assert.Equal("client", string(v.(ext.SpanKindEnum)))
+				case string(ext.DBStatement):
+					assert.Equal("GET --> SET", v)
+				default:
+					panic("unknown tag")
+				}
+			}
+		})
+	}
+}
+
+func TestHook_AfterProcessPipeline(t *testing.T) {
+	assert := assert.New(t)
+	opentracing.SetGlobalTracer(mocktracer.New())
+
+	tests := map[string]struct {
+		ctx  context.Context
+		hk   hook
+		cmds []redis.Cmder
+	}{
+		"success": {
+			ctx:  context.TODO(),
+			hk:   hook{addrs: []string{"127.0.0.1:6379", "127.0.0.1:6378"}, database: 10},
+			cmds: []redis.Cmder{redis.NewStringCmd("GET", "ALLAN"), redis.NewStringCmd("SET", "ALLAN")},
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			// before
+			newCtx, err := tc.hk.BeforeProcessPipeline(tc.ctx, tc.cmds)
+			assert.NoError(err)
+			assert.NoError(err)
+			now := time.Now()
+			time.Sleep(1 * time.Millisecond)
+
+			// after
+			err = tc.hk.AfterProcessPipeline(newCtx, tc.cmds)
+			assert.NoError(err)
+			span := opentracing.SpanFromContext(newCtx).(*mocktracer.MockSpan)
+			assert.True(span.FinishTime.UnixNano() > now.UnixNano())
+		})
+	}
+
+}
